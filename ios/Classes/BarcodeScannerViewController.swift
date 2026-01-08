5826cb8c06112242e0aef8e3b309f8498d6d5112
Vipin Kashyap <kaizen.labsindia@gmail.com>
2026-01-07 18:44:06 -0800

    fix(ios): dismiss scanner view correctly on cancel/complete
    Use presentingViewController to dismiss the modally-presented
    navigation controller instead of calling dismiss on the navigation
    controller itself, which doesn't work for modal presentations.

--- ios/Classes/BarcodeScannerViewController.swift
+++ ios/Classes/BarcodeScannerViewController.swift
@@ -213,12 +213,12 @@ class BarcodeScannerViewController: UIViewController {
   
   private func errorResult(errorCode: String){
     delegate?.didFailWithErrorCode(self, errorCode: errorCode)
-    self.navigationController?.dismiss(animated: false)
+    self.navigationController?.presentingViewController?.dismiss(animated: false)
   }
-  
+
   private func scanResult(_ scanResult: ScanResult){
     self.delegate?.didScanBarcodeWithResult(self, scanResult: scanResult)
-    self.navigationController?.dismiss(animated: false)
+    self.navigationController?.presentingViewController?.dismiss(animated: false)
   }
   
   private func mapRestrictedBarcodeTypes() -> [String] {
